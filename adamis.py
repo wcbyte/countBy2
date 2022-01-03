8254e86d8c81457b35d726a9f61dba298001a2a1
Cameron Wiley <83432568+wcbyte@users.noreply.github.com>
2022-01-03 13:07:21 -0500

    Update adamis.py

--- adamis.py
+++ adamis.py
@@ -1,11 +1,6 @@
-# Create a function to count from
-# 0 to 100 by counting by twos
+def count():
+    for i in range(0, 100):
+        if i%2 == 0:
+            print(i)
 
-# ---------------- Create a function - Jacob (Best) ----------------
-def countingBy2Jacob():
-    variable = 2
-    while variable < 100:
-        print(variable)
-        variable += 2
-
-countingBy2Jacob()
+count()
