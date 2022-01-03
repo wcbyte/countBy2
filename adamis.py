62724512cdcb87e4d1ffd916cbc0b2a38008c71a
adamit24 <taylorreneeadami@gmail.com>
2022-01-03 12:41:39 -0500

    Create adamis.py

---
+++ adamis.py
@@ -0,0 +1,11 @@
+# Create a function to count from
+# 0 to 100 by counting by twos
+
+# ---------------- Create a function - Jacob (Best) ----------------
+def countingBy2Jacob():
+    variable = 2
+    while variable < 100:
+        print(variable)
+        variable += 2
+
+countingBy2Jacob()
